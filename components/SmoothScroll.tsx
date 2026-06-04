39a4faa65cdea4d28a74eb84fe431ee890957bce
gustavo <gustavo.silvaaa17@gmail.com>
2026-06-03 22:38:14 -0300

    adjust smooth scroll duration and easing function

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -5,11 +5,12 @@ import Lenis from 'lenis'
 export default function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 0.6,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -12 * t)),
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
+     
     })
 
     function raf(time: number) {
